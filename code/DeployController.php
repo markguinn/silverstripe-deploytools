711ddc84437e3dc38181a43de116cb387a04614d
Anselm Christophersen <ac@anselm.dk>
2013-03-25 19:04:03 +0100

    indentation (#1)

--- code/DeployController.php
+++ code/DeployController.php
@@ -6,238 +6,238 @@
  */
 class DeployController extends Controller
 {
-    static $allowed_actions = array('index', 'commit_hook', 'install', 'InstallForm');
-    static $log_path = '../logs'; // BASE_PATH is assumed
+	static $allowed_actions = array('index', 'commit_hook', 'install', 'InstallForm');
+	static $log_path = '../logs'; // BASE_PATH is assumed
 
-    /**
-     * Rejection
-     */
-    public function index() {
-        return $this->httpError(403);
-    }
-
-
-    /**
-     * @return string
-     */
-    public function install() {
-        if (!Permission::check('ADMIN')) return $this->httpError(403);
-
-        return $this->customise(array(
-            'Title'     => 'Install Deploy Tools',
-            'Content'   => '<p>Some instructions would be nice.</p>',
-            'Form'      => $this->InstallForm(),
-        ))->renderWith(array('Page','Page'));
-    }
+	/**
+	 * Rejection
+	 */
+	public function index() {
+		return $this->httpError(403);
+	}
 
 
-    /**
-     * @return Form
-     */
-    public function InstallForm() {
-        $fields = array();
+	/**
+	 * @return string
+	 */
+	public function install() {
+		if (!Permission::check('ADMIN')) return $this->httpError(403);
 
-        // find the repo slug and source
-        chdir(BASE_PATH);
-        exec(self::git() . ' remote -v', $out);
-        foreach ($out as $line) {
-            if (preg_match('#^origin\s+.*bitbucket\.org.(.+?)/(.+?)\.git#', $line, $matches)) {
-                $fields[] = HiddenField::create('RepoUser', '', $matches[1]);
-                $fields[] = HiddenField::create('RepoSlug', '', $matches[2]);
-                $fields[] = LiteralField::create('repo', "<p>You appear to be deploying from Bitbucket ({$matches[1]}/{$matches[2]}.git). If you enter your username and password below we will set up a service hook to deploy automatically. Your credentials will not be logged or saved.</p>");
-                $fields[] = TextField::create('PostURL', 'Commit Hook URL (must be unique to this server)', self::default_hook_url());
-                $fields[] = TextField::create('ApiUser', 'Bitbucket Username');
-                $fields[] = PasswordField::create('ApiPassword', 'Bitbucket Password');
-                break;
-            }
-        }
-
-        // TODO: checkboxes to backup assets and database
-        // TODO: backup destination options - local file, ftp, sftp, cloudfiles
-
-        $actions = array(
-            FormAction::create('doInstall', 'Install')
-        );
-
-        return new Form($this, 'InstallForm', FieldList::create($fields), FieldList::create($actions));
-    }
+		return $this->customise(array(
+			'Title'     => 'Install Deploy Tools',
+			'Content'   => '<p>Some instructions would be nice.</p>',
+			'Form'      => $this->InstallForm(),
+		))->renderWith(array('Page','Page'));
+	}
 
 
-    /**
-     * @param $data
-     * @param $form
-     * @return string
-     */
-    public function doInstall($data, $form) {
-        if (!Permission::check('ADMIN')) return $this->httpError(403);
-        $actions = array();
+	/**
+	 * @return Form
+	 */
+	public function InstallForm() {
+		$fields = array();
 
-        if (isset($data['ApiUser']) && !empty($data['ApiUser']) && !empty($data['ApiPassword'])) {
-            $postURL = Director::absoluteURL(empty($data['PostURL']) ? self::default_hook_url() : $data['PostURL']);
+		// find the repo slug and source
+		chdir(BASE_PATH);
+		exec(self::git() . ' remote -v', $out);
+		foreach ($out as $line) {
+			if (preg_match('#^origin\s+.*bitbucket\.org.(.+?)/(.+?)\.git#', $line, $matches)) {
+				$fields[] = HiddenField::create('RepoUser', '', $matches[1]);
+				$fields[] = HiddenField::create('RepoSlug', '', $matches[2]);
+				$fields[] = LiteralField::create('repo', "<p>You appear to be deploying from Bitbucket ({$matches[1]}/{$matches[2]}.git). If you enter your username and password below we will set up a service hook to deploy automatically. Your credentials will not be logged or saved.</p>");
+				$fields[] = TextField::create('PostURL', 'Commit Hook URL (must be unique to this server)', self::default_hook_url());
+				$fields[] = TextField::create('ApiUser', 'Bitbucket Username');
+				$fields[] = PasswordField::create('ApiPassword', 'Bitbucket Password');
+				break;
+			}
+		}
 
-            $json = file_get_contents('https://' . urlencode($data['ApiUser']) . ':' . urlencode($data['ApiPassword']) . '@api.bitbucket.org/1.0/repositories/' . $data['RepoUser'] . '/' . $data['RepoSlug'] . '/services');
-            $services = $json ? json_decode($json, true) : array();
+		// TODO: checkboxes to backup assets and database
+		// TODO: backup destination options - local file, ftp, sftp, cloudfiles
 
-            // if services already exist, make sure there's not already one for this site
-            if ($services && count($services) > 0) {
-                foreach ($services as $service) {
-                    if ($service['service']['type'] == 'POST'
-                            && $service['service']['fields'][0]['value'] == $postURL) {
-                        $postURL = false; // indicate that we don't need a new one
-                    }
-                }
-            }
+		$actions = array(
+			FormAction::create('doInstall', 'Install')
+		);
 
-            // assuming we need to, post the request
-            if ($postURL) {
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_URL, 'https://' . urlencode($data['ApiUser']) . ':' . urlencode($data['ApiPassword']) . '@api.bitbucket.org/1.0/repositories/' . $data['RepoUser'] . '/' . $data['RepoSlug'] . '/services');
-                curl_setopt($ch, CURLOPT_POST, 1);
-                curl_setopt($ch, CURLOPT_POSTFIELDS, 'type=POST&URL=' . urlencode($postURL));
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                $server_output = curl_exec ($ch);
-                $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-                curl_close ($ch);
-                if ($http_status == 200 || $http_status == 201) {
-                    $actions[] = 'Bitbucket POST service for deployment was created.';
-                } else {
-                    $actions[] = 'Bitbucket POST service failed. Response code=' . $http_status;
-                }
-            } else {
-                $actions[] = 'Bitbucket POST service was not created because it already exists.';
-            }
-        }
-
-        return $this->customise(array(
-            'Title'     => 'Deploy Tools Install',
-            'Content'   => '<p>The following actions were completed:</p><ul><li>'
-                            . implode('</li><li>', $actions) . '</li></ul>',
-        ))->renderWith(array('Page','Page'));
-    }
+		return new Form($this, 'InstallForm', FieldList::create($fields), FieldList::create($actions));
+	}
 
 
-    /**
-     * Handle commit hook post from bitbucket or github
-     * @return string
-     */
-    public function commit_hook() {
-        if (isset($_POST['payload'])) {
-            // github and bitbucket use a 'payload' parameter
-            $json = $_POST['payload'];
-        } else {
-            $json = file_get_contents('php://input');
-        }
-        if (!$json) return 'ignored';
+	/**
+	 * @param $data
+	 * @param $form
+	 * @return string
+	 */
+	public function doInstall($data, $form) {
+		if (!Permission::check('ADMIN')) return $this->httpError(403);
+		$actions = array();
 
-        $data = ($json) ? json_decode($json, true) : null;
-        if (!$data || !is_array($data['commits'])) return 'ignored';
+		if (isset($data['ApiUser']) && !empty($data['ApiUser']) && !empty($data['ApiPassword'])) {
+			$postURL = Director::absoluteURL(empty($data['PostURL']) ? self::default_hook_url() : $data['PostURL']);
 
-        // look through the commits
-        $found = false;
-        $tags = array();
-        foreach ($data['commits'] as $commit) {
-            if (preg_match('/\[deploy(:.+)?\]/', $commit['message'], $matches)) {
-                $found = true;
-                if (count($matches) > 1 && $matches[1] != '') {
-                    $tags[] = substr($matches[1], 1);
-                } else {
-                    $tags[] = 'live';
-                }
-            }
-        }
+			$json = file_get_contents('https://' . urlencode($data['ApiUser']) . ':' . urlencode($data['ApiPassword']) . '@api.bitbucket.org/1.0/repositories/' . $data['RepoUser'] . '/' . $data['RepoSlug'] . '/services');
+			$services = $json ? json_decode($json, true) : array();
 
-        if (!$found) return 'ignored';
-        if (defined('DEPLOY_TAG') && !in_array(DEPLOY_TAG, $tags)) return 'ignored';
+			// if services already exist, make sure there's not already one for this site
+			if ($services && count($services) > 0) {
+				foreach ($services as $service) {
+					if ($service['service']['type'] == 'POST'
+							&& $service['service']['fields'][0]['value'] == $postURL) {
+						$postURL = false; // indicate that we don't need a new one
+					}
+				}
+			}
 
-        // create the deployment
-        increase_time_limit_to(600);
-        $deploy = new Deploy(BASE_PATH, array(
-            'log'   => BASE_PATH . '/' . self::$log_path . '/deploy.log'
-        ));
+			// assuming we need to, post the request
+			if ($postURL) {
+				$ch = curl_init();
+				curl_setopt($ch, CURLOPT_URL, 'https://' . urlencode($data['ApiUser']) . ':' . urlencode($data['ApiPassword']) . '@api.bitbucket.org/1.0/repositories/' . $data['RepoUser'] . '/' . $data['RepoSlug'] . '/services');
+				curl_setopt($ch, CURLOPT_POST, 1);
+				curl_setopt($ch, CURLOPT_POSTFIELDS, 'type=POST&URL=' . urlencode($postURL));
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				$server_output = curl_exec ($ch);
+				$http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+				curl_close ($ch);
+				if ($http_status == 200 || $http_status == 201) {
+					$actions[] = 'Bitbucket POST service for deployment was created.';
+				} else {
+					$actions[] = 'Bitbucket POST service failed. Response code=' . $http_status;
+				}
+			} else {
+				$actions[] = 'Bitbucket POST service was not created because it already exists.';
+			}
+		}
 
-        $deploy->post_deploy = function() use ($deploy) {
-            global $_FILE_TO_URL_MAPPING;
+		return $this->customise(array(
+			'Title'     => 'Deploy Tools Install',
+			'Content'   => '<p>The following actions were completed:</p><ul><li>'
+							. implode('</li><li>', $actions) . '</li></ul>',
+		))->renderWith(array('Page','Page'));
+	}
 
-            // composer install if detected
-            if (file_exists(BASE_PATH . DIRECTORY_SEPARATOR . 'composer.json')) {
-                if (file_exists('/usr/local/bin/composer')) {
-                    exec('composer install', $output);
-                    $deploy->log('Executing composer install...' . implode("\n", $output));
-                } else {
-                    $deploy->log('composer.json detected but unable to locate composer.');
-                }
-            }
 
-            // clear cache
-            $deploy->log('Clearing cache...');
-            DeployController::clear_cache();
+	/**
+	 * Handle commit hook post from bitbucket or github
+	 * @return string
+	 */
+	public function commit_hook() {
+		if (isset($_POST['payload'])) {
+			// github and bitbucket use a 'payload' parameter
+			$json = $_POST['payload'];
+		} else {
+			$json = file_get_contents('php://input');
+		}
+		if (!$json) return 'ignored';
 
-            // update database
-            if (isset($_FILE_TO_URL_MAPPING[BASE_PATH])) {
-                exec('php framework/cli-script.php dev/build', $output2);
-                $deploy->log('Updating database...' . implode("\n", $output2));
-            } else {
-                $deploy->log('Database not updated. $_FILE_TO_URL_MAPPING must be set for '.BASE_PATH);
-            }
+		$data = ($json) ? json_decode($json, true) : null;
+		if (!$data || !is_array($data['commits'])) return 'ignored';
+
+		// look through the commits
+		$found = false;
+		$tags = array();
+		foreach ($data['commits'] as $commit) {
+			if (preg_match('/\[deploy(:.+)?\]/', $commit['message'], $matches)) {
+				$found = true;
+				if (count($matches) > 1 && $matches[1] != '') {
+					$tags[] = substr($matches[1], 1);
+				} else {
+					$tags[] = 'live';
+				}
+			}
+		}
+
+		if (!$found) return 'ignored';
+		if (defined('DEPLOY_TAG') && !in_array(DEPLOY_TAG, $tags)) return 'ignored';
+
+		// create the deployment
+		increase_time_limit_to(600);
+		$deploy = new Deploy(BASE_PATH, array(
+			'log'   => BASE_PATH . '/' . self::$log_path . '/deploy.log'
+		));
+
+		$deploy->post_deploy = function() use ($deploy) {
+			global $_FILE_TO_URL_MAPPING;
+
+			// composer install if detected
+			if (file_exists(BASE_PATH . DIRECTORY_SEPARATOR . 'composer.json')) {
+				if (file_exists('/usr/local/bin/composer')) {
+					exec('composer install', $output);
+					$deploy->log('Executing composer install...' . implode("\n", $output));
+				} else {
+					$deploy->log('composer.json detected but unable to locate composer.');
+				}
+			}
+
+			// clear cache
+			$deploy->log('Clearing cache...');
+			DeployController::clear_cache();
+
+			// update database
+			if (isset($_FILE_TO_URL_MAPPING[BASE_PATH])) {
+				exec('php framework/cli-script.php dev/build', $output2);
+				$deploy->log('Updating database...' . implode("\n", $output2));
+			} else {
+				$deploy->log('Database not updated. $_FILE_TO_URL_MAPPING must be set for '.BASE_PATH);
+			}
 
 //    		SS_ClassLoader::instance()->getManifest()->regenerate();
 //            ob_start();
 //            DatabaseAdmin::create()->doBuild(false, true, false);
 //            $deploy->log('dev/build complete: '.ob_get_contents());
 //            ob_end_clean();
-        };
+		};
 
-        $deploy->execute();
-        return 'ok';
-    }
-
-
-    /**
-     * Clears all the cache
-     * @static
-     */
-    public static function clear_cache() {
-   		$dirs = array('.', 'cache', 'aggregate_cache');
-
-   		foreach ($dirs as $dirname) {
-   			$dirpath = TEMP_FOLDER . '/' . $dirname;
-   			if ($dir = opendir($dirpath)) {
-   			    while (false !== ($file = readdir($dir))) {
-   			    	if ($file != '.' && $file != '..' && $file != '.svn' && !is_dir($file)) {
-   				        unlink($dirpath . '/' . $file);
-   			    	}
-                }
-   			    closedir($dir);
-   			}
-   		}
-   	}
+		$deploy->execute();
+		return 'ok';
+	}
 
 
-    /**
-     * Somewhat janky way to detect absolute git path.
-     * This could use some work.
-     *
-     * @return string
-     */
-    public static function git() {
-        $check = array(
-            '/Applications/Xcode.app/Contents/Developer/usr/bin/git',
-            '/usr/bin/git',
-            '/usr/local/bin/git',
-        );
+	/**
+	 * Clears all the cache
+	 * @static
+	 */
+	public static function clear_cache() {
+		$dirs = array('.', 'cache', 'aggregate_cache');
 
-        foreach ($check as $fn) {
-            if (file_exists($fn)) return $fn;
-        }
+		foreach ($dirs as $dirname) {
+			$dirpath = TEMP_FOLDER . '/' . $dirname;
+			if ($dir = opendir($dirpath)) {
+				while (false !== ($file = readdir($dir))) {
+					if ($file != '.' && $file != '..' && $file != '.svn' && !is_dir($file)) {
+						unlink($dirpath . '/' . $file);
+					}
+				}
+				closedir($dir);
+			}
+		}
+	}
 
-        return 'git';
-    }
+
+	/**
+	 * Somewhat janky way to detect absolute git path.
+	 * This could use some work.
+	 *
+	 * @return string
+	 */
+	public static function git() {
+		$check = array(
+			'/Applications/Xcode.app/Contents/Developer/usr/bin/git',
+			'/usr/bin/git',
+			'/usr/local/bin/git',
+		);
+
+		foreach ($check as $fn) {
+			if (file_exists($fn)) return $fn;
+		}
+
+		return 'git';
+	}
 
 
-    /**
-     * @return string
-     */
-    public static function default_hook_url() {
-        return Director::absoluteURL(DEPLOY_TOOLS_URL . '/commit-hook');
-    }
+	/**
+	 * @return string
+	 */
+	public static function default_hook_url() {
+		return Director::absoluteURL(DEPLOY_TOOLS_URL . '/commit-hook');
+	}
 }
